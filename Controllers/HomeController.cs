405b4f96c1fb28a3dc4d8f7bb2287502ed932baf
epicgamer <redditgang4life@yandex.ru>
2022-08-18 16:45:44 +0000

    select

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,23 +6,29 @@ using Beon.Models.ViewModels;
 
 namespace Beon.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly UserManager<BeonUser> _userManager;
+    public HomeController(UserManager<BeonUser> userManager)
     {
-        private readonly UserManager<BeonUser> _userManager;
-        public HomeController(UserManager<BeonUser> userManager) {
-            _userManager = userManager;
-        }
-
-        [HttpGet]
-        [Route("/")]
-        public IActionResult Index()
-        {
-            ICollection<UserProfileLinkViewModel> users = new List<UserProfileLinkViewModel>();
-            foreach(var u in _userManager.Users) {
-                users.Add(new UserProfileLinkViewModel(u.UserName, u.DisplayName));
-            }
-
-            return View(new HomeViewModel(users));
-        }
+      _userManager = userManager;
     }
+
+    [HttpGet]
+    [Route("/")]
+    public IActionResult Index()
+    {
+      ICollection<UserProfileLinkViewModel> users = new List<UserProfileLinkViewModel>();
+      var dt = _userManager.Users
+        .Select(u => new { u.UserName, u.DisplayName })
+        .ToList();
+
+      foreach (var u in dt)
+      {
+        users.Add(new UserProfileLinkViewModel(u.UserName, u.DisplayName));
+      }
+
+      return View(new HomeViewModel(users));
+    }
+  }
 }
