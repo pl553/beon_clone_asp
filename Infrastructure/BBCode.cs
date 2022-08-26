7b191184072b94144caeab056e3d3b334210f738
epicgamer <redditgang4life@yandex.ru>
2022-08-27 00:40:53 +0300

    imagelink bbtag

--- Infrastructure/BBCode.cs
+++ Infrastructure/BBCode.cs
@@ -7,7 +7,10 @@ namespace Beon.Infrastructure {
       new BBTag("B", "<b>", "</b>", 0),
       new BBTag("PRE", "<pre>", "</pre>", 1),
       new BBTag("CENTER", "<div style=\"text-align: center;\">", "</div>", 2),
-      new BBTag("RIGHT", "<div style=\"text-align: right;\">", "</div>", 3)
+      new BBTag("RIGHT", "<div style=\"text-align: right;\">", "</div>", 3),
+      new BBTag("IMG", "<img class=\"image-medium\" src=\"${content}\" />", "", false, true, 4, allowUrlProcessingAsText: false), 
+      new BBTag("IMGSMALL", "<img class=\"image-small\" src=\"${content}\" />", "", false, true, 5, allowUrlProcessingAsText: false), 
+      new BBTag("IMGLARGE", "<img class=\"image-large\" src=\"${content}\" />", "", false, true, 6, allowUrlProcessingAsText: false), 
     });
   }
 }
